1ae8fe68f8924874607b946429f8aae31b26e934
gokul bhatt <gokulbhatt05@gmail.com>
2016-12-03 12:02:31 +0545

    added categories app

--- categories/models.py
+++ categories/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class category(models.Model)
-    name=models.CharField(max_length=32)
-    description=models.CharField(max_length=120)
+    name= models.CharField(max_length=32)
+    description= models.CharField(max_length=120)
